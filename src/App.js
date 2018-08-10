d82d48993dc80fc9b6685265768a1ab108a45a47
Bonnie Jordan <thebonstergirl@gmail.com>
2018-08-10 16:12:44 -0500

    first problem finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import TopicBrowser from '../src/components/TopicBrowser/TopicBrowser';
 
 class App extends Component {
   render() {
+    return (<TopicBrowser />)
     
   }
 }
---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+class EvenAndOdd extends Component {
+    constructor(){
+        super();
+        this.state={ 
+            evenArray: [],
+            oddArray: [],
+            userInput: ""
+
+        }
+    this.solveMe = this.solveMe.bind(this);
+    this.inputHere = this.inputHere.bind(this);
+    }
+    inputHere(value){
+        return this.setState({ userInput: value});
+    }
+
+
+    solveMe = (userInput) => {
+        var arr = userInput.split(',');
+        var newEvens = [];
+        var newOdds = [];
+        for (let i =0; i < arr.length; i++){
+        if (arr[i]%2===0){
+            newEvens.push(arr[i] + ', ')
+        }
+        else{newOdds.push(arr[i] + ', ')};}
+        return this.setState({evenArray: newEvens, oddArray: newOdds});
+    }
+    
+        
+    render () {
+        return (
+        <div> 
+            <h4> Evens and Odds </h4>
+       <div className="puzzleBox evenAndOddPB">
+       <input className="inputLine" onChange={(e)=> this.inputHere(e.target.value)} />
+       <button className="confirmationButton" onClick={() => this.solveMe(this.state.userInput)}>Split</button>
+       <span className="resultsBox">Evens: {this.state.evenArray}</span>
+       <span className="resultsBox">Odds: {this.state.oddArray}</span>
+       </div>
+       </div>
+        )
+}
+}
+
+export default EvenAndOdd;
