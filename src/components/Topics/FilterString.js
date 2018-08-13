9a4ff337a9e0df834e1d673a7e730285e7a4be5b
Bonnie Jordan <thebonstergirl@gmail.com>
2018-08-13 08:47:52 -0500

    sum function created

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,42 @@
 import React, {Component} from 'react';
 
 class FilterString extends Component {
+    constructor(){
+        super();
+        this.state={
+            unFilteredArray: ["Bonnie", "Bree", "Brock", "John", "Kate", "Dalton", "Chris", "Kat", "Nate", "Trevor", "Alex", "Jeff", "Jason", "Paisley", "Pudge", "Daisy"],
+            userInput: "",
+            filteredArray: []
+        }
+        this.userInput = this.userInput.bind(this);
+        this.filterMe = this.filterMe.bind(this);
+    }
+
+    userInput = (prop) => {
+        this.setState({userInput: prop});
+    }
+
+    filterMe = (prop) => {
+        var unFilteredArray = this.state.unFilteredArray;
+        var string = "";
+
+        for (let i=0; i<unFilteredArray.length; i++){
+            if (unFilteredArray[i].includes(prop)){
+                string += `${unFilteredArray[i]} `;
+            }
+        } this.setState({filteredArray: string});
+    }
+
     render(){
         return(
-            <p> FilterString Component</p>
+            <div className="puzzleBox filterStringPB">
+            <h4>Filter String</h4>
+            <span classname="puzzleText">Unfiltered strings: {JSON.stringify(this.state.unFilteredArray, null, 10)}</span>
+            <input className="inputLine" onChange={(e)=> this.userInput(e.target.value)}/>
+            <button className="confirmationButton" onClick={()=> this.filterMe(this.state.userInput)}>Filter</button>
+            <span className="resultsBox filterStringRB">Filtered: {JSON.stringify(this.state.filteredArray, null, 10) }    </span>
+            
+            </div>
         )
     }
 }
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
 
 class Sum extends Component{
+    constructor(){
+        super();
+        this.state={number1: 0,
+        number2: 0,
+        sum: null}
+    }
+    number1(e){
+        this.setState({number1: parseInt(e, 10)});
+    }
+    number2(e){
+        this.setState({number2: parseInt(e, 10)});
+    }
+    add(num1, num2){
+        this.setState({sum: parseInt(num1, 10) + parseInt(num2, 10)});
+    }
     render(){
         return(
-            <p>Sum Component</p>
+            <div  className="puzzleBox SumPB"> 
+            <h4> Sum </h4>
+            <input className="inputLine" onChange={(e)=> this.state.number1=(e.target.value)} />
+            <input className="inputLine" onChange={(e)=> this.state.number2=(e.target.value)}/>
+            <button className="confirmationButton" onClick={()=> {this.add(this.state.number1, this.state.number2)}}>Add numbers </button>
+            <span className="resultsBox">{this.state.sum}</span>
+            </div>
         )
     }
 }
